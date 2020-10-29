81e6c483898a5bfd13cb69eaf326ab13cb43d2d0
Yevhen Terentiev <yevhen.terentiev@gmail.com>
2020-10-29 20:45:02 +0200

    npm scripts to build & start

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # kubernetes-dashboard-desktop-app
+
+## Run
+```
+npm run build
+npm start
+```
--- main.js
+++ main.js
@@ -2,18 +2,23 @@ const { app, BrowserWindow } = require('electron')
 const url = require("url");
 const path = require("path");
 const childProcess = require("child_process");
+const net = require("net");
 
-function createWindow () {
-    const binPath = path.join(__dirname, "dist/amd64 bin/dashboard")
+function runBackend () {
+    const binPath = path.join(__dirname, "dist/amd64/dashboard")
     childProcess.exec(`"${binPath}" --kubeconfig ~/.kube/config`, (error, stdout, stderr) => {
-        console.log(stdout, stderr, error)
+        if (error) {
+            console.error(error)
+        }
     })
+}
 
+function createWindow() {
     // Create the browser window.
     let win = new BrowserWindow({
         width: 1280,
         height: 1024,
-        icon: path.join(__dirname, 'dist/electron/assets/images/kubernetes-logo.png'),
+        icon: path.join(__dirname, 'dist/frontend/assets/images/kubernetes-logo.png'),
         webPreferences: {
             nodeIntegration: true
         }
@@ -22,7 +27,7 @@ function createWindow () {
     // and load the index.html of the app.
     win.loadURL(
         url.format({
-            pathname: path.join(__dirname, `dist/electron/index.html`),
+            pathname: path.join(__dirname, `dist/frontend/index.html`),
             protocol: "file:",
             slashes: true
         })
@@ -36,4 +41,9 @@ function createWindow () {
     })
 }
 
-app.whenReady().then(createWindow)
+runBackend()
+
+setTimeout(() => {app.whenReady().then(createWindow)}, 1000)
+
+
+
